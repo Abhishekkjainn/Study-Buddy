1aafa9c33a6dec6669036ed349678bd14588490e
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-12-05 23:33:59 +0530

    feat: restrict to vit domain

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -17,4 +17,8 @@ const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
+provider.setCustomParameters({
+  hd: "vitstudent.ac.in",
+});
+
 export { auth, provider };
