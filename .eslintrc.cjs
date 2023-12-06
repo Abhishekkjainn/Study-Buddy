e91dbf0e356230b73c62a2c8f36f0a617c80b549
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-12-06 17:32:09 +0530

    fix: build-errors and protect profile page

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@
 const config = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: true,
+    project: false,
   },
   plugins: ["@typescript-eslint"],
   extends: [
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,33 @@
-import React from "react";
+"use client";
+
+// import { getServerSession } from "next-auth/next"
+import { authOptions } from "~/lib/auth";
+import { useSession } from "next-auth/react";
 
 export default function Protected() {
-  return <div>Protected page, you are logged in!</div>;
+	const { data: session } = useSession();
+
+	if (typeof window === "undefined") return null;
+
+	if (session) {
+		return (
+			<>
+				<h1>Protected Page</h1>
+				<p>You can view this page because you are signed in.</p>
+			</>
+		);
+	}
+	return <p>Access Denied</p>;
 }
+
+// export async function getServerSideProps(context) {
+// 	return {
+// 	  props: {
+// 		session: await getServerSession(
+// 		  context.req,
+// 		  context.res,
+// 		  authOptions
+// 		),
+// 	  },
+// 	}
+//   }
